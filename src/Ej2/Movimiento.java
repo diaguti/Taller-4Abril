727b2a598a87b131ff5370586f7a62d09ef2d6b4
Estudiante <Estudiante@10.203.1.46>
2017-04-04 15:20:45 -0500

    Taller 4 abril

--- src/Ej2/Movimiento.java
+++ src/Ej2/Movimiento.java
@@ -5,13 +5,56 @@
  */
 package Ej2;
 
+import java.util.Date;
+
 /**
  *
  * @author Estudiante
  */
 public class Movimiento {
-    private String date;
+    private Date date;
     private double saldoAnterior;
     private double cantidad;
     private Tipo tipo;
+
+    public Movimiento(Date date, double saldoAnterior, double cantidad, Tipo tipo) {
+        this.date = date;
+        this.saldoAnterior = saldoAnterior;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getSaldoAnterior() {
+        return saldoAnterior;
+    }
+
+    public void setSaldoAnterior(double saldoAnterior) {
+        this.saldoAnterior = saldoAnterior;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+    
+    
 }
